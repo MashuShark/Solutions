fb550acfba3b6d340e76665fb7daa75391ad778e
Maryna Shulzhenko <us1gfw.maryna@gmail.com>
2021-11-04 17:42:08 +0200

    Add my first Solutions

---
+++ src/main/java/Integer.java
@@ -0,0 +1,37 @@
+public class Integer {
+
+    public static int Square(int num) {
+        return num*num;
+    }
+
+    /**
+     *input: byte
+     * output: Kilobyte/Megabyte/Gigabyte
+     */
+   public static int byteConversion(int Bytes, String conversion){
+       int KB = 1024;
+       int MB = 1048576;
+       int GB = 1073741824;
+
+       if (conversion == "KB"){
+           return (Bytes/KB);
+       } else if (conversion == "MB"){
+           return (Bytes/MB);
+       } else if (conversion == "GB") {
+           return (Bytes / GB);
+       } else return 0;
+
+   }
+
+    public static int Reverse(int number){
+        int first = number/100;
+        int second = (number/10)%10;
+        int third = number%10;
+
+        int reversNumber = third * 100 + second *10 + first;
+
+        return reversNumber;
+    }
+
+
+}
